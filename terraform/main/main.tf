7c4c4a3a5a3ebb7ef95cc3c8ba7341667cb2ac36
yeji <229eyzi@gmail.com>
2026-03-19 15:04:01 +0900

    fix: secrets k8s_namespace 변경(planit-)

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -228,7 +228,7 @@ module "secrets" {
 
   project_name          = var.project_name
   environment           = var.environment
-  k8s_namespace         = "planit"
+  k8s_namespace         = "planit-${var.environment}"
   oidc_provider_arn     = module.eks.oidc_provider_arn
   oidc_issuer           = module.eks.oidc_issuer
   db_username           = var.db_username
