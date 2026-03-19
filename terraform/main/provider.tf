016e1de9cbbc4fd0c6cae2376636cda7fa3193fe
jybill01 <jdragon0151@gmail.com>
2026-03-19 16:22:33 +0900

    fix provider2

--- terraform/main/provider.tf
+++ terraform/main/provider.tf
@@ -14,7 +14,7 @@ provider "aws" {
 
 provider "helm" {
   kubernetes = {
-    host                   = module.eks.cluster_endpoint
+    host                   = "https://localhost:8443"
     cluster_ca_certificate = base64decode(module.eks.cluster_ca_data)
     exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
