b5357d72e98f747abd3dd549632f275e6dc0868b
jybill01 <jdragon0151@gmail.com>
2026-03-19 16:08:43 +0900

    step fix2

--- terraform/main/provider.tf
+++ terraform/main/provider.tf
@@ -15,5 +15,14 @@ provider "aws" {
 provider "helm" {
   kubernetes = {
     config_path = "/home/runner/kubeconfig"
+    host = "https://localhost:8443"
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    insecure = true 
+
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      command     = "aws"
+    }
   }
 }
