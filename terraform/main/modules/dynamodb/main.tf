c5b3d90284dd7b334fbd9d077f3903341b036461
yeji <229eyzi@gmail.com>
2026-03-19 22:05:12 +0900

    aws_dynamodb_table 속성 설정

--- terraform/main/modules/dynamodb/main.tf
+++ terraform/main/modules/dynamodb/main.tf
@@ -1,10 +1,16 @@
 resource "aws_dynamodb_table" "this" {
   name         = var.table_name
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "id"
+  hash_key     = "PK"
+  range_key    = "SK"
 
   attribute {
-    name = "id"
+    name = "PK"
+    type = "S"
+  }
+
+  attribute {
+    name = "SK"
     type = "S"
   }
 
